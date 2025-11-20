a8c0d98ee486f2e62c0f52c03550784d9fcea3b2
HangFamily <hangquoctai.family@gmail.com>
2025-11-20 08:37:21 +0700

    Update Home v1

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -16,7 +16,7 @@ const MainLayout = ({ children }) => {
             <ul className='*:py-3! *:px-1!'>
               <li><a className='text-white! block text-base!' href="/">🏠 Trang tổng</a></li>
               <li><a className='text-white! block text-base!' href="#">⭐ Giới thiệu</a></li>
-              <li><a className='text-white! block text-base!' href="#">📑 Hướng dẫn sử dụng</a></
+              <li><a className='text-white! block text-base!' href="#">📑 Hướng dẫn sử dụng</a></li>
             </ul>
           </div>
           <Footer className='bg-transparent! h-fit! p-2! *:text-white text-xs! space-y-1'>
